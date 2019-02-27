857bf7d16b13ddfe22e9f07aaf1e3bc60fce10e7
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-02-27 16:19:41 -0700

    CSS added, Numbers upon click displaying to the box location

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+
+// Go back and watch the saturday video, Q went over an assignment that is like the homework assignment
+
+// code everything in sections, one line at a time, focus on one thing. Dont try to do it all at once.
+
+
+// create an alert
+
+
+
+
